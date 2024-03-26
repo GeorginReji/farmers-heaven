512ae37cbceaf310167f777b3688201f2ad50a03
GeorginReji <georginreji@gamil.com>
2024-03-26 23:46:48 +0530

    removed supbase dependency

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,13 +6,7 @@ export default defineNuxtConfig({
     '@/assets/scss/main.scss',
     '@/assets/scss/elements-custom.scss'
   ],
-  supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-    redirect: false
-    },
   modules: [
-    '@element-plus/nuxt',
-    '@nuxtjs/supabase'
+    '@element-plus/nuxt'
   ]
 })
