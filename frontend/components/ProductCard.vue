e9dcc1d70c017488219bf5d5460a4dd95cda8522
GeorginReji <georginreji@gamil.com>
2024-06-02 22:58:28 +0530

    cart page completed

--- frontend/components/ProductCard.vue
+++ frontend/components/ProductCard.vue
@@ -1,26 +1,28 @@
 <template>
-  <el-card shadow="hover">
-    <img src="../assets/images/spice-1.jpg" class="image">
-    <div class="card-content">
-      <div class="product-details">
-        <div class="product-name">
-          <NuxtLink to="/Product">
-            <span>Star anise</span>
-          </NuxtLink>
+  <div class="product-card-wrapper">
+    <el-card shadow="hover">
+      <img src="../assets/images/spice-1.jpg" class="image">
+      <div class="card-content">
+        <div class="product-details">
+          <div class="product-name">
+            <NuxtLink to="/Product">
+              <span>Star anise</span>
+            </NuxtLink>
+          </div>
+          <p class="description">Lorem ipsum dolor, sit amet dicta, eligendi ipsam, deserunt dolorum necessitatibus
+            officiis
+            voluptate atque quaerat.
+          </p>
         </div>
-        <p class="description">Lorem ipsum dolor, sit amet dicta, eligendi ipsam, deserunt dolorum necessitatibus
-          officiis
-          voluptate atque quaerat.
-        </p>
-      </div>
-      <div class="btn-price">
-        <span>₹250</span>
-        <div class="bottom">
-          <el-button type="success" class="button">Add to <i class="ri-shopping-cart-line"></i></el-button>
+        <div class="btn-price">
+          <span>₹250</span>
+          <div class="bottom">
+            <el-button type="success" class="button">Add to <i class="ri-shopping-cart-line"></i></el-button>
+          </div>
         </div>
       </div>
-    </div>
-  </el-card>
+    </el-card>
+  </div>
 </template>
 
 <script setup>
@@ -28,48 +30,61 @@
 </script>
 
 <style lang="scss">
-.el-card__body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  background-color: #FFFBF0;
-  &:hover {
-    background-color: #fff1de;
-  }
-}
-.card-content {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  span {
-    font-size: medium;
-    font-weight: 600;
-  }
-  p {
-    padding-bottom: 10px;
-    font-size: small;
-  }
-  .product-details {
-    min-width: 63%;
-    .product-name {
-      padding: 1rem 0;
-    }
-  }
-  .btn-price {
+.product-card-wrapper {
+  .el-card__body {
     display: flex;
     align-items: center;
+    justify-content: center;
     flex-direction: column;
-    gap: 10px;
-    span {
-      font-weight: 400;
-      font-size: 2rem;
+    background-color: #FFFBF0;
+    &:hover {
+      background-color: #fff1de;
     }
   }
+  .card-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    span {
+      font-size: 1.2em;
+      font-weight: 600;
+    }
+    p {
+      padding-bottom: 10px;
+      font-size: 1em;
+    }
+    .product-details {
+      // min-width: 63%;
+      .product-name {
+        padding: 1rem 0;
+      }
+    }
+    .btn-price {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      gap: 1rem;
+      span {
+        font-weight: 400;
+        font-size: 1em;
+      }
+    }
+  }
+  .button {
+    i {
+      padding-left: 5px;
+    }
+  }
+  .el-card__body {
+    padding: 1rem;
+  }
 }
-.button {
-  i {
-    padding-left: 5px;
+@media(max-width: 767px) {
+  .card-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
   }
 }
 </style>
