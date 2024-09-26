8c300e4702352cc065f8e8157e0dff05c112ec49
GeorginReji <georginreji@gmail.com>
2024-09-26 15:55:52 +0530

    path spec error fix

--- pages/Admin/ProductView.vue
+++ pages/Admin/ProductView.vue
@@ -1,7 +1,12 @@
 <template>
-	<div>
-		<h1>Product View</h1>
-	</div>
+	<el-container>
+		<el-row :gutter="20">
+			<el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
+			<el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
+			<el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
+			<el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
+		</el-row>
+	</el-container>
 </template>
 
 <script setup>
