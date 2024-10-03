1d5139c1db964873afb2fe1954574e166b924fd1
GeorginReji <georginreji@gmail.com>
2024-10-03 16:18:08 +0530

    Add menu components structure

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,18 @@
 <template>
-	<el-container class="layout">
+	<el-container>
 		<el-container class="navbar-section">
-			<!-- <topbar /> -->
-			<navbar />
+			<MenuTopBar />
 		</el-container>
 		<el-main class="hero-section">
 			<slot />
 		</el-main>
 		<div class="whats-app-container">
 			<a href="https://wa.me/8281560102">
-				<i class="ri-whatsapp-line"></i>
+				<i class="ri-whatsapp-line" />
 			</a>
 		</div>
 		<el-footer class="footer-section">
-			<navFooter />
+			<MenuFooter />
 		</el-footer>
 	</el-container>
 </template>
@@ -21,41 +20,30 @@
 <script setup></script>
 
 <style lang="scss" scoped>
-.layout {
-	// display: flex;
-	// flex-direction: column;
-	// justify-content: space-between;
-	.navbar-section {
-		position: fixed;
-		// display: flex;
-		// flex-direction: column;
-		// justify-content: space-between;
-		// top: 0;
-		// left: 0;
-		width: 100%;
-		z-index: 100;
-	}
-	.hero-section {
-		margin-top: 25px;
-		// width: 100%;
-		background-color: #fffbf0;
-	}
-	.whats-app-container {
-		position: fixed;
-		bottom: 20px;
-		right: 20px;
-		width: 50px;
-		height: 50px;
-		border-radius: 50%;
-		background-color: #25d366;
-		text-align: center;
-		font-size: 30px;
-		line-height: 50px;
-		z-index: 100;
-		a {
-			text-decoration: none;
-			color: white;
-		}
+.navbar-section {
+	position: fixed;
+	width: 100%;
+	z-index: 100;
+}
+.hero-section {
+	margin-top: 25px;
+	background-color: #fffbf0;
+}
+.whats-app-container {
+	position: fixed;
+	bottom: 20px;
+	right: 20px;
+	width: 50px;
+	height: 50px;
+	border-radius: 50%;
+	background-color: #25d366;
+	text-align: center;
+	font-size: 30px;
+	line-height: 50px;
+	z-index: 100;
+	a {
+		text-decoration: none;
+		color: white;
 	}
 }
 </style>
